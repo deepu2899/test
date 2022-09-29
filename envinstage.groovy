e816ea5b919ab077b987f027788e1e6d6ed4db32
deepu2899 <108063853+deepu2899@users.noreply.github.com>
2022-09-29 22:47:32 +0530

    Create envinstage.groovy

---
+++ envinstage.groovy
@@ -0,0 +1,25 @@
+pipeline {
+  agent any
+
+  environment {
+    // git will be available in entire pipeline
+    git = "PIPELINE"
+  }
+
+  stages {
+    stage("local") {
+      environment {
+        // BAR will only be available in this stage
+        demo = "STAGE"
+      }
+      steps {
+        sh 'echo "git is $git and demo is $git"'
+      }
+    }
+    stage("global") {
+      steps {
+          sh 'echo "git is $git and demo is $git"'
+      }
+    }
+  }
+}
